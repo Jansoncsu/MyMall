afc8a01fb6ded8d157eb39874eca4f72153ea6ed
Janson <jian_sheng_tan@163.com>
2017-12-06 22:20:05 +0800

    User Model Complete

---
+++ src/main/java/com/mymall/controller/backend/UserManagerController.java
@@ -0,0 +1,34 @@
+package com.mymall.controller.backend;
+
+
+import com.mymall.common.Const;
+import com.mymall.common.ServerResponse;
+import com.mymall.pojo.User;
+import com.mymall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/user")
+public class UserManagerController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+            User user = response.getData();
+            if(user.getRole() == Const.Role.ROLE_ADMIN){
+                session.setAttribute(Const.CURRENT_USER,user);
+                return response;
+            }else{
+                return ServerResponse.createByErrorMessage("不是管理员,无法登录");
+            }
+        }
+        return response;
+    }
+}
